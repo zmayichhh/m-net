774aed93a94cb5c03f83b89bff902bb60633a27c
zmayichhh <smajic96@hotmail.com>
2023-12-07 17:23:08 +0100

    a

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 6000,
+  env: {
+    url: "https://www.m-net.de/privatkunden/",
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: "cypress/integration/examples/*.js",
+    setupNodeEvents,
   },
 });
